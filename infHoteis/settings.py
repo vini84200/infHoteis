dfe9f47fdbd5eb53cdf95c86f96b3f4b2c717b5a
Vinicius J. F. <www.vini84200@hotmail.com>
2023-12-18 12:33:58 -0300

    END-23 Preparar deploy

--- infHoteis/settings.py
+++ infHoteis/settings.py
@@ -45,8 +45,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
