74b39847bab718dafcc7ca957fc1546cd95dda71
Vinicius J. F. <www.vini84200@hotmail.com>
2024-01-28 17:11:50 -0300

    END-28 API da reserva - Endpoint

--- hoteis/admin.py
+++ hoteis/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from hoteis.models import Hotel, Beneficio, Quarto, CategoriaQuarto
+from hoteis.models import Hotel, Beneficio, Quarto, CategoriaQuarto, Reserva
+
 
 class QuartoInline(admin.TabularInline):
     model = Quarto
@@ -11,7 +12,14 @@ class HotelAdmin(admin.ModelAdmin):
         QuartoInline,
     ]
 
+
 admin.site.register(Hotel, HotelAdmin)
 admin.site.register(Beneficio)
 admin.site.register(Quarto)
 admin.site.register(CategoriaQuarto)
+
+
+@admin.register(Reserva)
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = ['cliente', 'quarto', 'data_inicio', 'data_fim', 'hospedes', 'preco', 'pago', 'checkin', 'checkout',
+                    'cancelada']
--- hoteis/urls.py
+++ hoteis/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from hoteis.views import HotelViewSet
+from hoteis.views import HotelViewSet, ReservaViewSet
 
 router = DefaultRouter()
 
 router.register('hoteis', HotelViewSet)
+router.register('reservas', ReservaViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
