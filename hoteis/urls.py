4712cfed23acbbc3d89dfdae7f5ed865b81e5e81
Guilherme Terres <guirterres@gmail.com>
2024-01-28 15:51:26 -0300

    Add view and router

--- hoteis/urls.py
+++ hoteis/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from hoteis.views import HotelViewSet
+from hoteis.views import HotelViewSet, CategoriaQuartoViewSet
 
 router = DefaultRouter()
 
 router.register('hoteis', HotelViewSet)
+router.register('categoria', CategoriaQuartoViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
--- hoteis/views.py
+++ hoteis/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
-from hoteis.models import Hotel
-from hoteis.serializers import HotelSerializer
-
+from hoteis.models import Hotel, CategoriaQuarto
+from hoteis.serializers import HotelSerializer, CategoriaSerializer
 
 class ReadOnly(BasePermission):
     def has_permission(self, request, view):
@@ -14,5 +13,11 @@ class ReadOnly(BasePermission):
 class HotelViewSet(viewsets.ModelViewSet):
     queryset = Hotel.objects.all()
     serializer_class = HotelSerializer
+    authentication_classes = []
+    permission_classes = [ReadOnly]
+
+class CategoriaQuartoViewSet(viewsets.ModelViewSet):
+    queryset = CategoriaQuarto.objects.all()
+    serializer_class = CategoriaSerializer
     authentication_classes = []
     permission_classes = [ReadOnly]
