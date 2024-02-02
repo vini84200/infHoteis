c2d821798cf5df7f9525b46662c2b3270034951d
julia-ms <mombachdasilva@gmail.com>
2024-02-02 17:16:06 -0300

    continuei o front da reserva

--- inf_hoteis_frontend/src/app/detalhes/page.tsx
+++ inf_hoteis_frontend/src/app/detalhes/page.tsx
@@ -1,24 +1,22 @@
-import { Button, Dropdown, Space, Input, Flex, Divider, Col, Row, Form, Slider } from 'antd';
-import { WifiOutlined, CoffeeOutlined, CarOutlined } from '@ant-design/icons';
+import { Button, Dropdown, Space, Input, Flex, Divider, Col, Row, Form, Slider, Image } from 'antd';
+import { WifiOutlined, CoffeeOutlined, CarOutlined, ClockCircleOutlined } from '@ant-design/icons';
 import React, { useState } from 'react';
+import './Reserva.css';
+import minhaImagem from './hotel2.png';
 
-const Reserva = () => {
-    const pageStyle = {
-      backgroundColor: 'black',
-      color: 'white',
-      minHeight: '100vh', 
-      padding: '20px', 
-      fontFamily: 'montserrat'
-    }
+declare module "*.jpg";
+
+const Reserva = () => {    
   
     return (
-      <div style={pageStyle}>
+      <div className="pageStyle">
         <Row>
           <Col span={12}>
+            <div className="columns">
             <p style={{fontSize: '40px'}}>Faça sua reserva!</p>
             <div style={{border: '5px'}}>
               <div style={{fontSize: '30px'}}>
-                  <p >Passo 1: </p>
+                  <p >Passo 1: Verifique sua reserva</p>
               </div>
                 <p >Comodidades:</p>
                 <Space>
@@ -29,20 +27,25 @@ const Reserva = () => {
                   <CarOutlined />
                   <p>Estacionamento</p>
                 </Space>
-                <p>Escolha opção de cama:</p>
+
+                <Space>
+                  <ClockCircleOutlined />
+                  <p>Check-in:</p>
+                </Space>
+                
                 
               </div>
 
             <div style={{border: '5px'}}>
               <div style={{fontSize: '30px'}}>
-                  <p >Passo 2: dados pessoais</p>
+                  <p >Passo 2: Dados pessoais</p>
               </div>
                 <p >Nome completo:</p>
-                <Input placeholder="Jane Doe" variant="filled" style={{ backgroundColor: 'grey' }}/>
+                <Input placeholder="Jane Doe" variant="filled" className="inputStyle"/>
                 <p >Email:</p>
-                <Input placeholder="exemplo@email.com" variant="filled" style={{ backgroundColor: 'grey' }}/>
+                <Input placeholder="exemplo@email.com" variant="filled" className="inputStyle"/>
                 <p >Telefone:</p>
-                <Input placeholder="99 99999-9999" variant="filled" style={{ backgroundColor: 'grey' }}/>
+                <Input placeholder="99 99999-9999" variant="filled" className="inputStyle"/>
               </div>
             
               <div style={{border: '5px'}}>
@@ -50,35 +53,41 @@ const Reserva = () => {
                   <p >Passo 3: Pagamento</p>
               </div>
                 <p >Nome no cartão:</p>
-                <Input placeholder="JANE DOE" variant="filled" style={{ backgroundColor: 'grey' }}/>
+                <Input placeholder="JANE DOE" variant="filled" className="inputStyle"/>
                 <p >Numero do cartão</p>
-                <Input placeholder="9999 9999 9999 9999" variant="filled" style={{ backgroundColor: 'grey' }}/>
-                <p >Data de Validade:</p>
-                <Input placeholder="11/11" variant="filled" style={{ backgroundColor: 'grey' }}/>
-                <p >Código de segurança:</p>
-                <Input placeholder="999" variant="filled" style={{ backgroundColor: 'grey' }}/>
+                <Input placeholder="9999 9999 9999 9999" variant="filled" className="inputStyle"/>
+                <Space>
+                  <div>
+                    <p >Data de Validade:</p>
+                    <Input placeholder="11/11" variant="filled" className="inputStyle"/>
+                  </div>
+                  <div>
+                    <p >Código de segurança:</p>
+                    <Input placeholder="999" variant="filled" className="inputStyle"/>
+                  </div>
+                </Space>
+              </div>
               </div>
             </Col>
 
             <Col span={12}>
-            <img
-              src={require('../../assets/fotoQuarto.jpg').default}
-              alt="quarto"
-              style={{ width: '300px', height: '200px' }}
-            />
+            <div className="columns">
+              <div className="imageBorder">
+                
+                  <img src={minhaImagem.src} alt="quarto" width={'80%'} height={'50%'} style={{ borderRadius: '10px' }}/>
+                
+
+              </div>
+              
               <Button type="primary" style={{ backgroundColor: 'grey', borderColor: 'grey' }}>
                       Fazer Reserva
               </Button>
+              </div>
             </Col>
           </Row>
-        
-
-       
         
       </div>
     );
   };
   
   export default Reserva;
-
-  
