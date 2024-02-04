7f99f6674e0adde64851af69e9540d79e64fcdcf
Vinicius J. F. <www.vini84200@hotmail.com>
2024-02-03 23:15:58 -0300

    Imagens e novo modelo

--- hoteis/urls.py
+++ hoteis/urls.py
@@ -1,7 +1,9 @@
+from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from hoteis.views import HotelViewSet, ReservaViewSet
+from infHoteis import settings
 
 router = DefaultRouter()
 
@@ -9,5 +11,5 @@ router.register('hoteis', HotelViewSet)
 router.register('reservas', ReservaViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
-]
+                  path('', include(router.urls))
+              ]
