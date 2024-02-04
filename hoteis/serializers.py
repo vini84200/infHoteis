67d31b51fdcdeca5d1099df2a9e94e733f4d013c
Vinicius J. F. <www.vini84200@hotmail.com>
2024-02-04 11:12:41 -0300

    Fazer reservas em grupos

--- hoteis/serializers.py
+++ hoteis/serializers.py
@@ -1,6 +1,9 @@
-from rest_framework import serializers
+import datetime
+from logging import info
 
+from rest_framework import serializers
 from hoteis.models import Hotel, Quarto, Beneficio, CategoriaQuarto, Reserva
+from rest_framework.validators import ValidationError
 
 
 class BeneficioSerializer(serializers.ModelSerializer):
@@ -33,10 +36,67 @@ class HotelSerializer(serializers.ModelSerializer):
         fields = ['id', 'nome', 'descricao', 'estado', 'cidade', 'rua', 'imagem', 'avaliacao', 'quarto_set']
 
 
+class ReservaRequestSerializer(serializers.Serializer):
+    class QuartoReservaSerializer(serializers.Serializer):
+        tipo = serializers.PrimaryKeyRelatedField(queryset=CategoriaQuarto.objects.all())
+        qtde = serializers.IntegerField(
+            min_value=1,
+        )
+
+    reserva = serializers.ListField(child=QuartoReservaSerializer(), min_length=1)
+    hotel = serializers.PrimaryKeyRelatedField(queryset=Hotel.objects.all())
+    # reserva = QuartoReservaSerializer(many=True)
+    data_inicio = serializers.DateField()
+    data_fim = serializers.DateField()
+    cliente = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    def validate(self, attrs):
+        if attrs['data_inicio'] > attrs['data_fim']:
+            raise ValidationError('Data de início maior que a data de fim')
+        if attrs['data_inicio'] < datetime.date.today():
+            raise ValidationError('Data de início no passado')
+
+        for reserva in attrs['reserva']:
+            tipo_quarto = reserva['tipo']
+            qtde = reserva['qtde']
+            hotel = attrs['hotel']
+
+            disponiveis = hotel.disponiveis_em(attrs['data_inicio'], attrs['data_fim'], tipo_quarto)
+
+            if qtde > len(disponiveis):
+                raise ValidationError(f'Quarto {tipo_quarto.nome} não tem {qtde} disponíveis')
+
+        return attrs
+
+    def create(self, validated_data):
+        print("Criando reserva")
+        reservas = []
+        try:
+            for reserva in validated_data['reserva']:
+                tipo_quarto = reserva['tipo']
+                qtde = reserva['qtde']
+                hotel = validated_data['hotel']
+                novas = hotel.reservar(
+                    validated_data['data_inicio'],
+                    validated_data['data_fim'],
+                    tipo_quarto,
+                    qtde,
+                    validated_data['cliente']
+                )
+                reservas.extend(novas)
+        except ValueError as e:
+            for reserva in reservas:
+                # Deletar as reservas já feitas
+                info(f'Deletando reserva {reserva}')
+                reserva.delete()
+            raise ValidationError(e)
+        return reservas
+
+
 class ReservaSerializer(serializers.ModelSerializer):
     # quarto = QuartoSerializer(read_only=True)
 
     class Meta:
         model = Reserva
-        fields = ['id', 'quarto', 'cliente', 'data_inicio', 'data_fim', 'hospedes', 'preco', 'pago', 'checkin',
+        fields = ['id', 'quarto', 'cliente', 'data_inicio', 'data_fim', 'preco', 'pago', 'checkin',
                   'checkout', 'cancelada']
--- hoteis/views.py
+++ hoteis/views.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 from rest_framework.response import Response
 
 from hoteis.models import Hotel, Reserva, Quarto
-from hoteis.serializers import HotelSerializer, ReservaSerializer
+from hoteis.serializers import HotelSerializer, ReservaSerializer, ReservaRequestSerializer
 
 
 class ReadOnly(BasePermission):
@@ -14,11 +14,10 @@ class ReadOnly(BasePermission):
         return request.method in SAFE_METHODS
 
 
-class HotelViewSet(viewsets.ModelViewSet):
+class HotelViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Hotel.objects.all()
     serializer_class = HotelSerializer
     authentication_classes = []
-    permission_classes = [ReadOnly]
 
 
 class ReservaPermission(BasePermission):
@@ -33,41 +32,25 @@ class ReservaPermission(BasePermission):
         return obj.cliente == request.user or request.user.is_staff
 
 
+
 class ReservaViewSet(viewsets.ModelViewSet):
     queryset = Reserva.objects.all()
     serializer_class = ReservaSerializer
     permission_classes = [ReservaPermission]
 
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return ReservaRequestSerializer
+        if self.request.method == 'POST':
+            return ReservaRequestSerializer
+        return super().get_serializer_class()
+
     def create(self, request, *args, **kwargs):
-        request.data['cliente'] = request.user.id
-        # Verificar se não está no passado
-        if datetime.date.today() > datetime.datetime.strptime(request.data['data_inicio'], '%Y-%m-%d').date():
-            return Response({'detail': 'Data de início no passado'}, status=status.HTTP_400_BAD_REQUEST)
-        # Verificar se o fim é depois do início
-        if datetime.datetime.strptime(request.data['data_inicio'], '%Y-%m-%d').date() >= datetime.datetime.strptime(
-                request.data['data_fim'], '%Y-%m-%d').date():
-            return Response({'detail': 'Data de fim antes da data de início'}, status=status.HTTP_400_BAD_REQUEST)
+        reserva = self.get_serializer(data=request.data)
+        reserva.is_valid(raise_exception=True)
+        reservas = reserva.create(reserva.validated_data)
 
-        # Verificar se quarto está disponível
-        # Isso é feito verificando se existe alguma reserva que começa antes do fim e termina depois do início
-        if Reserva.objects.filter(quarto=request.data['quarto'], data_inicio__lte=request.data['data_fim'],
-                                  data_fim__gte=request.data['data_inicio'], cancelada=False).exists():
-            return Response({'detail': 'Quarto não disponível'}, status=status.HTTP_400_BAD_REQUEST)
-
-        # Calcular preço
-        quarto = Quarto.objects.get(id=request.data['quarto'])
-        # request.data['quarto'] = {'id': quarto.id}
-        request.data['hospedes'] = quarto.categoria.hospedes
-        duracao = (datetime.datetime.strptime(request.data['data_fim'], '%Y-%m-%d').date() - datetime.datetime.strptime(
-            request.data['data_inicio'], '%Y-%m-%d').date()).days
-        request.data['preco'] = quarto.categoria.preco * duracao
-
-        request.data['pago'] = False
-        request.data['checkin'] = False
-        request.data['checkout'] = False
-        request.data['cancelada'] = False
-
-        return super().create(request, *args, **kwargs)
+        return Response(ReservaSerializer(reservas, many=True).data, status=status.HTTP_201_CREATED)
 
     def destroy(self, request, *args, **kwargs):
         reserva = self.get_object()
@@ -87,4 +70,3 @@ class ReservaViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Reserva.objects.filter(cliente=self.request.user)
-
